68709d17b5c4419ce737d1e0f779e606766bcab0
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-06 14:25:04 -0500

    created AppLoading.js for app loading screen

---
+++ AppLoading.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { Asset, AppLoading } from 'expo';
+
+export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Image source={require('./assets/images/function-earth-splash.png')} />
+      </View>
+    );
+  }
+
+  async _cacheResourcesAsync() {
+    const images = [
+      require('./assets/images/function-earth-splash.png')
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages)
+
+  }
+}

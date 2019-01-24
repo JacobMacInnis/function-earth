47ee4036f04b6ee359c3f5c8a9154bd0c389a9b8
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-24 08:47:59 -0500

    GlobalStatsScreen responsive updates

--- src/components/GlobalImpact.js
+++ src/components/GlobalImpact.js
@@ -1,49 +1,67 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import RF from "react-native-responsive-fontsize";
 export default class GlobalImpact extends React.Component {
   
   render() {
     let myStats = 
-    <View style={{height: 175}}>
-      <View style={{flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 10, padding: 2}}>
-        <Text style={{flex: 2, fontSize: 18}}>Earth:</Text> 
-        <Text style={{flex: 1, textAlign: 'right', fontSize: 18}}>{this.props.earth}</Text>
-        </View>
-      <View style={{flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 10, padding: 2}}>
-        <Text style={{flex: 2, fontSize: 18}}>Ocean:</Text> 
-        <Text style={{flex: 1, textAlign: 'right', fontSize: 18}}>{this.props.ocean}</Text>
+    <View style={styles.myStatsContainer}>
+      <View style={styles.lineContainer}>
+        <Text style={styles.textLeft}>Earth:</Text> 
+        <Text style={styles.textRight}>{this.props.earth}</Text>
       </View>
-      <View style={{flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 10, padding: 2}}>
-        <Text style={{flex: 2, fontSize: 18}}>Animal:</Text> 
-        <Text style={{flex: 1, textAlign: 'right', fontSize: 18}}>{this.props.animal}</Text>
+      <View style={styles.lineContainer}>
+        <Text style={styles.textLeft}>Ocean:</Text> 
+        <Text style={styles.textRight}>{this.props.ocean}</Text>
       </View>
-      <View style={{flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 10, padding: 2}}>
-        <Text style={{flex: 2, fontSize: 18}}>Humanity:</Text> 
-        <Text style={{flex: 1, textAlign: 'right', fontSize: 18}}>{this.props.humanity}</Text>
+      <View style={styles.lineContainer}>
+        <Text style={styles.textLeft}>Animal:</Text> 
+        <Text style={styles.textRight}>{this.props.animal}</Text>
       </View>
-      <View style={{flex: 1,flexWrap: 'nowrap',flexDirection: 'row', borderWidth: 1, borderRadius: 10, height: 15, padding: 2}}>
-        <Text style={{flex: 2, fontSize: 20, fontWeight: 'bold'}}>Total {this.props.title}:</Text> 
-        <Text style={{flex: 1, textAlign: 'right', fontSize: 20, fontWeight: 'bold'}}>{this.props.all}</Text>
+      <View style={styles.lineContainer}>
+        <Text style={styles.textLeft}>Humanity:</Text> 
+        <Text style={styles.textRight}>{this.props.humanity}</Text>
+      </View>
+      <View style={styles.lineContainerLast}>
+        <Text style={styles.textLeftLast}>Total {this.props.title}:</Text> 
+        <Text style={styles.textRightLast}>{this.props.all}</Text>
       </View>
     </View>
     return (
-      <View style={{borderWidth: 1, borderRadius: 10, padding: 10, width: 250}}>
-        <Text style={{fontSize: 20, fontWeight: 'bold', alignSelf: 'center'}}>Global {this.props.title}</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>Global {this.props.title}</Text>
         {myStats}
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1, borderRadius: 10, padding: 10, width: wp('70%')
+  },
+  header: {
+    fontSize: RF(4), fontWeight: 'bold', alignSelf: 'center'
+  },
+  myStatsContainer: {
+    height: hp('26%')
+  },
+  lineContainer: {
+    flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 10, padding: 2
+  },
+  lineContainerLast: {
+    flex: 1,flexWrap: 'nowrap',flexDirection: 'row', height: 15, padding: 2
+  },
   textLeft: {
-    flex: 1,
-    fontSize: 16,
-    alignSelf: 'flex-start'
+    flex: 2, fontSize: RF(3)
   },
   textRight: {
-    flex: 1,
-    fontSize: 16,
-    alignSelf: 'flex-start'
+    flex: 1, textAlign: 'right', fontSize: RF(3)
   },
+  textLeftLast: {
+    flex: 2, fontSize: RF(3.2), fontWeight: 'bold'
+  },
+  textRightLast: {
+    flex: 1, textAlign: 'right', fontSize: RF(3.2), fontWeight: 'bold'
+  }
 });

aedc1b9ebb461c423fa5ede725c6fcdbb7fef941
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-24 07:40:21 -0500

    Entries.js responsive sizing and text

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import RF from "react-native-responsive-fontsize";
 let type = {
   earth: 'Earth Entry',
   ocean: 'Ocean Entry',
@@ -16,14 +18,14 @@ class Entries extends Component {
 
   render() {
     return (
-      <View style={{height: 180, width: 260, marginLeft: (this.props.index === 0 ? 70 : 10), borderWidth: 1, borderRadius: 6, borderColor: '#34495e', overflow: 'hidden'}}>
+      <View style={{height: hp('35%'), width: wp('85%'), marginLeft: (this.props.index === 0 ? wp('7%') : wp('4%')), borderWidth: 1, borderRadius: 6, borderColor: '#34495e', overflow: 'hidden'}}>
         <View style={{flex: 2, backgroundColor: this.props.color, alignItems: 'center'}}>
-          <Text style={{fontSize: 18, fontWeight: '700', color: 'white', marginTop: 5}}>{type[this.props.type]}</Text>
-          <Text style={{fontSize: 15, color: 'white'}}>{this.props.timeStamp}</Text>
-          <Text style={{fontSize: 16, fontWeight: '400', color: 'white'}}>{this.props.stateRegion}:{this.props.country}</Text>
+          <Text style={{fontSize: RF(3.5), fontWeight: '700', color: 'white', marginTop: 5}}>{type[this.props.type]}</Text>
+          <Text style={{fontSize: RF(2.8), color: 'white'}}>{this.props.timeStamp}</Text>
+          <Text style={{fontSize: RF(2.8), fontWeight: '400', color: 'white'}}>{this.props.stateRegion}:{this.props.country}</Text>
         </View>
         <View style={{flex: 3, backgroundColor: secondColor[this.props.type], alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{fontSize: 18, padding: 8}}>{this.props.entry}</Text>
+          <Text style={{fontSize: RF(3.1), padding: 8}}>{this.props.entry}</Text>
         </View>
       </View>
     );

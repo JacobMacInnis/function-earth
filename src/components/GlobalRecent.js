70c2ab7b1ec98b4f06cde7c37de222ab31f8a701
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-24 07:14:32 -0500

    GlobalRecent.js removed unused code

--- src/components/GlobalRecent.js
+++ src/components/GlobalRecent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native';
 import Entries from './Entries';
 const colors = {
   earth: 'green',

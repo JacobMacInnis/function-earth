0f495554558b772cc4eb591f0f369eec600d639d
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-25 09:08:30 -0500

    added react-native-responsive-screen to TopUserImpact

--- src/components/Top-User-Impact.js
+++ src/components/Top-User-Impact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import RF from 'react-native-responsive-fontsize';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default class TopUserIMpact extends React.Component {
   

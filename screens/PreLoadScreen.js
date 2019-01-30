8065a8d18d95ab9c51931580a193cfe90a2b4b9e
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-30 16:49:34 -0500

    authentication working

--- screens/PreLoadScreen.js
+++ screens/PreLoadScreen.js
@@ -6,6 +6,9 @@ import { setAuthToken, refreshAuthToken } from '../src/actions/auth'
 
 
 class PreLoadScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
   constructor(props) {
     super(props);
     this.state = {

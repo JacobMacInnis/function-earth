f30bf2a5957b2e0598c72127fb809f6b3b1922ee
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-01-31 14:02:50 -0500

    removed unused code from store.js

--- src/store.js
+++ src/store.js
@@ -2,8 +2,6 @@ import { AsyncStorage } from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
-// import { loadAuthToken } from './async-storage';
-import { setAuthToken, refreshAuthToken } from './actions/auth'
 
 // Import Reducers
 import registrationReducer from './reducers/registration';
